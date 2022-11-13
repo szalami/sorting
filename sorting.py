28f904e4968b0dc7cd423a43dd90403164b2202b
szalami <szalami72@gmail.com>
2022-11-13 22:08:43 +0100

    Kész

--- sorting.py
+++ sorting.py
@@ -1,7 +1,6 @@
-nums = [87, 23, 44, 100, 0, 12, 1, 65, 43, 51, 99, 74]
+nums = [87, 23, 44, 100, 0, 12, 1, 65, 43, 51, 99, 74] 
 
-
-temp = 0
+temp = 0                     
 lenght = len(nums)
 for i in range(lenght):
     for j in range(0, lenght-i-1):
@@ -10,7 +9,7 @@ for i in range(lenght):
             nums[j] = nums[j+1]
             nums[j+1] = temp
             
-            
+print("Növekvő sorrendbe rendezett lista:")          
 print(nums)
             
                     
